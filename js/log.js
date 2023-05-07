05c6c5e31c403648651cb2616481a653a62a51b9
drinverse <aldrinanuver@yahoo.com>
2023-05-07 11:54:21 +0800

    Added dashboard in main.html

---
+++ js/log.js
@@ -0,0 +1,44 @@
+const Logs = [
+    {
+        logCategory : 'Company',
+        logRefNo : '1-050123-1',
+        logInteraction : 'Add',
+        logDate : '05/01/2023'
+    },
+    {
+        logCategory : 'Application',
+        logRefNo : '2-050223-2',
+        logInteraction : 'Edit',
+        logDate : '05/02/2023'
+    },
+    {
+        logCategory : 'Inspection',
+        logRefNo : '3-050323-3',
+        logInteraction : 'Delete',
+        logDate : '05/03/2023'
+    },
+    {
+        logCategory : 'NOV',
+        logRefNo : '4-050423-1',
+        logInteraction : 'Add',
+        logDate : '05/04/2023'
+    },
+    {
+        logCategory : 'NAF',
+        logRefNo : '5-050523-2',
+        logInteraction : 'Edit',
+        logDate : '05/05/2023'
+    },
+    {
+        logCategory : 'Company',
+        logRefNo : '1-050623-3',
+        logInteraction : 'Delete',
+        logDate : '05/06/2023'
+    },
+    {
+        logCategory : 'Application',
+        logRefNo : '2-050723-1',
+        logInteraction : 'Add',
+        logDate : '05/07/2023'
+    }    
+];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', function() {
+
+// -- JAVASCRIPT FOR OPENING MODAL --
+
 // Get the modal
 var modal = document.getElementById("modal-reg");
 
@@ -23,3 +27,79 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+})
+
+window.addEventListener('load', function() {
+
+// -- JAVASCRIPT FOR MAIN.HTML --
+
+// -- JAVASCRIPT FOR SIDEBAR --
+const sideMenu = document.querySelector("nav");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+// SHOW SIDEDAR
+menuBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'block';
+})
+
+// CLOSE SIDEBAR
+closeBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'none';
+})
+
+window.addEventListener('resize', () => {
+  sideMenu.style.display = 'block';
+}); 
+
+})
+
+window.addEventListener('load', function() {
+
+  // CHANGE ACTIVE IN SIDEBAR
+const sidebarToggler = document.querySelector("side-1");
+
+sidebarToggler.addEventListener('click', () => {
+  //document.body.classList.toggle('nav .sidebar a.active');
+
+  sidebarToggler.querySelector('a').classList.toggle('active');
+
+})
+
+})
+
+window.addEventListener('load', function() {
+
+  // CHANGE TO DARK THEME
+const themeToggler = document.querySelector(".theme-toggler");
+
+themeToggler.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme-variables');
+
+  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+})
+
+
+
+
+window.addEventListener('load', function() {
+// FILL ORDERS IN TABLE
+Logs.forEach(log => {
+  const tr = document.createElement('tr');
+  const trContent = `
+                  <td>${log.logCategory}</td>
+                  <td>${log.logRefNo}</td>
+                  <td class="${log.logInteraction ===
+                  'Delete' ? 'danger' : log.logInteraction ===
+                  'Edit' ? 'warning' : 'success'}">${log.logInteraction}</td>
+                  <td>${log.logDate}</td>
+                  <td class="primary">Details</td>
+                                `;
+  tr.innerHTML = trContent;
+  document.querySelector('table tbody').appendChild(tr);
+})
+})
