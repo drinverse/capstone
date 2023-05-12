e528a637bb7c612c4bc1eff1043cd821e8a913ec
drinverse <aldrinanuver@yahoo.com>
2023-05-12 22:23:25 +0800

    Added forms in main.html and Added functions in script.js

--- js/script.js
+++ js/script.js
@@ -30,6 +30,8 @@ window.onclick = function(event) {
 
 })
 
+
+
 window.addEventListener('load', function() {
 
 // -- JAVASCRIPT FOR MAIN.HTML --
@@ -49,26 +51,183 @@ closeBtn.addEventListener('click', () => {
   sideMenu.style.display = 'none';
 })
 
-window.addEventListener('resize', () => {
+/* window.addEventListener('resize', () => {
   sideMenu.style.display = 'block';
-}); 
+});  */
+
+// SIDEBAR SHOW WHEN IT RESIZE TO SPECIFIC WIDTH
+var limitFunc = function(){
+  if (window.innerWidth>769){
+     sideMenu.style.display = 'block';
+  }
+  else {
+    sideMenu.style.display = 'none';
+  }
+};
+
+window.addEventListener("resize", limitFunc);
+
+
+// CLOSE SIDEBAR WHEN CLICK OUTSIDE
+function closeNav() {
+  document.querySelector('nav').style.display = "none";
+}
+
+document.querySelector('main').addEventListener('click', closeNav)
+  
 
 })
+
+
 
 window.addEventListener('load', function() {
 
-  // CHANGE ACTIVE IN SIDEBAR
-const sidebarToggler = document.querySelector("side-1");
+  // SIDEBAR MENU TOGGLER
+  //let sidebarMenu = document.querySelector('.sidebar');
+  const childBar1 = document.querySelector('#bar-child-1');
+  const childBar2 = document.querySelector('#bar-child-2');
+  const childBar3 = document.querySelector('#bar-child-3');
+  const childBar4 = document.querySelector('#bar-child-4');
+  const childBar5 = document.querySelector('#bar-child-5');
+  const childBar6 = document.querySelector('#bar-child-6');
+  const childBar7 = document.querySelector('#bar-child-7');
 
-sidebarToggler.addEventListener('click', () => {
-  //document.body.classList.toggle('nav .sidebar a.active');
+  const dashboardSect = document.querySelector('#dashboard');
+  const companySect = document.querySelector('#company-section');
+  const applicationSect = document.querySelector('#application-section');
+  const inspectionSect = document.querySelector('#inspection-section');
+  const NOVSect = document.querySelector('#nov-section');
+  const NAFSect = document.querySelector('#naf-section');
+  
+  childBar1.onclick = () => {
+    childBar1.classList.add('active');
+    childBar2.classList.remove('active');
+    childBar3.classList.remove('active');
+    childBar4.classList.remove('active');
+    childBar5.classList.remove('active');
+    childBar6.classList.remove('active');
+    childBar7.classList.remove('active');
 
-  sidebarToggler.querySelector('a').classList.toggle('active');
+    dashboardSect.style.display = 'block';
+    companySect.style.display = 'none';
+    applicationSect.style.display = 'none';
+    inspectionSect.style.display = 'none';
+    NOVSect.style.display = 'none';
+    NAFSect.style.display = 'none';
+    
+  };
+
+  childBar2.onclick = () => {
+    childBar1.classList.remove('active');
+    childBar2.classList.add('active');
+    childBar3.classList.remove('active');
+    childBar4.classList.remove('active');
+    childBar5.classList.remove('active');
+    childBar6.classList.remove('active');
+    childBar7.classList.remove('active');
+
+    dashboardSect.style.display = 'none';
+    companySect.style.display = 'block';
+    applicationSect.style.display = 'none';
+    inspectionSect.style.display = 'none';
+    NOVSect.style.display = 'none';
+    NAFSect.style.display = 'none';
+
+  };
+
+  childBar3.onclick = () => {
+    childBar1.classList.remove('active');
+    childBar2.classList.remove('active');
+    childBar3.classList.add('active');
+    childBar4.classList.remove('active');
+    childBar5.classList.remove('active');
+    childBar6.classList.remove('active');
+    childBar7.classList.remove('active');
+
+    dashboardSect.style.display = 'none';
+    companySect.style.display = 'none';
+    applicationSect.style.display = 'block';
+    inspectionSect.style.display = 'none';
+    NOVSect.style.display = 'none';
+    NAFSect.style.display = 'none';
+
+  };
+
+  childBar4.onclick = () => {
+    childBar1.classList.remove('active');
+    childBar2.classList.remove('active');
+    childBar3.classList.remove('active');
+    childBar4.classList.add('active');
+    childBar5.classList.remove('active');
+    childBar6.classList.remove('active');
+    childBar7.classList.remove('active');
+
+    dashboardSect.style.display = 'none';
+    companySect.style.display = 'none';
+    applicationSect.style.display = 'none';
+    inspectionSect.style.display = 'block';
+    NOVSect.style.display = 'none';
+    NAFSect.style.display = 'none';
+
+  };
+
+  childBar5.onclick = () => {
+    childBar1.classList.remove('active');
+    childBar2.classList.remove('active');
+    childBar3.classList.remove('active');
+    childBar4.classList.remove('active');
+    childBar5.classList.add('active');
+    childBar6.classList.remove('active');
+    childBar7.classList.remove('active');
+
+    dashboardSect.style.display = 'none';
+    companySect.style.display = 'none';
+    applicationSect.style.display = 'none';
+    inspectionSect.style.display = 'none';
+    NOVSect.style.display = 'block';
+    NAFSect.style.display = 'none';
+    
+  };
+
+  childBar6.onclick = () => {
+    childBar1.classList.remove('active');
+    childBar2.classList.remove('active');
+    childBar3.classList.remove('active');
+    childBar4.classList.remove('active');
+    childBar5.classList.remove('active');
+    childBar6.classList.add('active');
+    childBar7.classList.remove('active');
+
+    dashboardSect.style.display = 'none';
+    companySect.style.display = 'none';
+    applicationSect.style.display = 'none';
+    inspectionSect.style.display = 'none';
+    NOVSect.style.display = 'none';
+    NAFSect.style.display = 'block';
+  };
+
+  childBar7.onclick = () => {
+    childBar1.classList.remove('active');
+    childBar2.classList.remove('active');
+    childBar3.classList.remove('active');
+    childBar4.classList.remove('active');
+    childBar5.classList.remove('active');
+    childBar6.classList.remove('active');
+    childBar7.classList.add('active');
+
+    dashboardSect.style.display = 'none';
+    companySect.style.display = 'none';
+    applicationSect.style.display = 'none';
+    inspectionSect.style.display = 'none';
+    NOVSect.style.display = 'none';
+    NAFSect.style.display = 'none';
+  };
 
 })
 
-})
 
+
+// LITEMODE AND DARKMODE
 window.addEventListener('load', function() {
 
   // CHANGE TO DARK THEME
@@ -85,7 +244,7 @@ themeToggler.addEventListener('click', () => {
 
 
 
-
+// TABLE CONTENT DEPENDING FROM LOG.JS
 window.addEventListener('load', function() {
 // FILL ORDERS IN TABLE
 Logs.forEach(log => {
